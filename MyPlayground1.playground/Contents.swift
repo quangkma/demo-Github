31f3e7e7fb4e1608a1d8ab07defe02e119400671
admin <admin@MacBook-Pro.local>
2019-01-08 20:48:23 +0700

    Lam them duoc bai tao mang chan le

--- MyPlayground1.playground/Contents.swift
+++ MyPlayground1.playground/Contents.swift
@@ -1,21 +1,42 @@
-import UIKit
+//import UIKit
+//
+//var str = "Hello, playground"
+//// chua co gi
+//
+//
+//// Check Nguyen to
+//func checkPrime(number: Int) -> Bool {
+//    if number < 2 {
+//        return false
+//    }
+//
+//    for i in 2..<number {
+//        if number % i == 0 {
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//print(checkPrime(number: 2))
+//
+//
+////
 
-var str = "Hello, playground"
-// chua co gi
 
-
-// Check Nguyen to
-func checkPrime(number: Int) -> Bool {
-    if number < 2 {
-        return false
-    }
+// Bai tap tao mang chan le nho 10
+func getChanLe() -> (chan: [Int], le: [Int]) {
+    var chan = [Int]()
+    var le = [Int]()
     
-    for i in 2..<number {
-        if number % i == 0 {
-            return false
+    for i in 0...10 {
+        if i % 2 == 0{
+            chan.append(i)
+        } else {
+            le.append(i)
         }
     }
-    return true
+    return (chan, le)
 }
 
-print(checkPrime(number: 2))
+print(getChanLe())
