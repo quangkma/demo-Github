ca07b5e096665ef5baf7787d7110d02db229d090
admin <admin@MacBook-Pro.local>
2019-01-08 20:42:09 +0700

    da lam bai check nguyen to

--- MyPlayground1.playground/Contents.swift
+++ MyPlayground1.playground/Contents.swift
@@ -2,3 +2,20 @@ import UIKit
 
 var str = "Hello, playground"
 // chua co gi
+
+
+// Check Nguyen to
+func checkPrime(number: Int) -> Bool {
+    if number < 2 {
+        return false
+    }
+    
+    for i in 2..<number {
+        if number % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+print(checkPrime(number: 2))
